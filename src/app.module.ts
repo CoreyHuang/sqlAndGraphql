4da1dd4e11df1e186ce67e97cff31fffa07c8d2c
corey <simon8888xsimon8888x@gmail.com>
2022-05-01 21:58:40 +0800

    graphql init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule, Query, Resolver } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Resolver()
+export class FooResolver {
+  @Query(() => String)
+  sayHello(): string {
+    return 'Hello World!';
+  }
+}
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FooResolver],
 })
-export class AppModule {}
+export class AppModule { }
